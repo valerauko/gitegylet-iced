a7307306993589903e879442fe1ba76b0a385b6b
Vale <valerauko@gmail.com>
2020-05-31 21:46:32 +0900

    Rename type

--- src/main.rs
+++ src/main.rs
@@ -5,30 +5,32 @@ use std::collections::{BinaryHeap, HashSet};
 use chrono::{DateTime, NaiveDateTime, TimeZone, Local};
 use std::cmp::{Ordering};
 
-struct WrappedCommit<'a> {
+struct ListedCommit<'a> {
     commit: Commit<'a>
 }
 
-impl Ord for WrappedCommit<'_> {
+impl Ord for ListedCommit<'_> {
     fn cmp(&self, other: &Self) -> Ordering {
         self.commit.time().cmp(&other.commit.time())
     }
 }
 
-impl PartialOrd for WrappedCommit<'_> {
+impl PartialOrd for ListedCommit<'_> {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
         Some(self.cmp(other))
     }
 }
 
-impl PartialEq for WrappedCommit<'_> {
+impl PartialEq for ListedCommit<'_> {
     fn eq(&self, other: &Self) -> bool {
         self.commit.id() == other.commit.id()
     }
 }
 
+impl Eq for ListedCommit<'_> {}
+
 struct CommitList<'repo> {
-    tree: BinaryHeap<WrappedCommit<'repo>>,
+    tree: BinaryHeap<ListedCommit<'repo>>,
     ids: HashSet<Oid>
 }
 
@@ -40,7 +42,7 @@ impl CommitList<'_> {
         branches.map(|branch| branch.unwrap().0.get().peel_to_commit().unwrap())
                 .for_each(|commit| {
                     ids.insert(commit.id());
-                    tree.push(WrappedCommit { commit: commit });
+                    tree.push(ListedCommit { commit: commit });
                 });
 
         CommitList {
@@ -51,9 +53,9 @@ impl CommitList<'_> {
 }
 
 impl <'repo>Iterator for CommitList<'repo> {
-    type Item = WrappedCommit<'repo>;
+    type Item = ListedCommit<'repo>;
 
-    fn next(&mut self) -> Option<WrappedCommit<'repo>> {
+    fn next(&mut self) -> Option<ListedCommit<'repo>> {
         if self.tree.is_empty() {
             return None
         }
@@ -61,14 +63,12 @@ impl <'repo>Iterator for CommitList<'repo> {
         first.commit.parents().for_each(|parent| {
             if !self.ids.contains(&parent.id()) {
                 self.ids.insert(parent.id());
-                self.tree.push(WrappedCommit { commit: parent });
+                self.tree.push(ListedCommit { commit: parent });
             }
         });
         Some(first)
     }
 }
-
-impl Eq for WrappedCommit<'_> {}
 
 fn arg_to_path() -> String {
     std::env::args().nth(1).unwrap_or(".".to_string())
